e7ed59943e46332a94766358665bbd06ae48f9dc
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-05-15 19:02:58 +0530

    Scafold: created a skelton strcture of monorepo project

---
+++ apps/backend/jest.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
